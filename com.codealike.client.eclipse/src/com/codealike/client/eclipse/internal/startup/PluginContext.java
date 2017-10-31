0509993222bfd4a1fa6d4f7f19dde8ef8f3ce59e
Daniel Iglesias <daniel.iglesias@outlook.com.ar>
2017-10-31 09:25:13 -0300

    Version changed to 0.4.2

--- com.codealike.client.eclipse/src/com/codealike/client/eclipse/internal/startup/PluginContext.java
+++ com.codealike.client.eclipse/src/com/codealike/client/eclipse/internal/startup/PluginContext.java
@@ -44,7 +44,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 @SuppressWarnings("restriction")
 public class PluginContext {
 
-	public static final String VERSION = "0.4.1";
+	public static final String VERSION = "0.4.2";
 	private static final String PLUGIN_PREFERENCES_QUALIFIER = "com.codealike.client.eclipse";
 	private static PluginContext _instance;
 	
