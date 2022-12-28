7421e7c90fa35ba7dfb0041801197d580d2bcb62
Paulo Vitor Magacho <pvmagacho@gmail.com>
2022-12-28 20:06:05 -0300

    Added executor

--- com.codealike.client.eclipse/src/com/codealike/client/eclipse/internal/services/BaseService.java
+++ com.codealike.client.eclipse/src/com/codealike/client/eclipse/internal/services/BaseService.java
@@ -2,20 +2,28 @@ package com.codealike.client.eclipse.internal.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public abstract class BaseService {
-	private final List<ServiceListener> listeners = new ArrayList<>();
-	
-	public void addListener(ServiceListener listener) {
-		listeners.add(listener);
-	}
+public abstract class BaseService implements Runnable {
+    private final ExecutorService ex  = Executors.newSingleThreadExecutor();
 
-	// Use proper access modifier
-	protected void publishEvent() {
-		System.out.println(this + " is goint to publish to " + listeners + " listeners.");
-		for (ServiceListener listener : listeners) {
-			listener.onEvent();
-		}
-	}
+    private final List<ServiceListener> serviceListeners = new ArrayList<ServiceListener>();
 
+    public void addListener(ServiceListener serviceListener) {
+        serviceListeners.add(serviceListener);
+    }
+
+    // Use proper access modifier
+    protected void publishEvent() {
+        ex.submit(this);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this + " is going to publish to " + serviceListeners + " listeners.");
+        for (ServiceListener serviceListener : serviceListeners) {
+            serviceListener.onEvent();
+        }
+    }
 }
